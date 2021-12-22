00a93f34d9694bc167c8661e61ca7037fe94393a
LOSAFABIO <fabio.losa95@gmail.com>
2021-12-22 12:09:17 +0100

    con modifiche mattina per le bombe

--- myscript.js
+++ myscript.js
@@ -7,7 +7,12 @@ play.addEventListener(`click`, function(){
     let livello = livelli.value;
     contenitore.innerHTML = " ";
     
+    const gameOver = false;
 
+    const bombeTotali      = 16;
+
+    const listaPosizioniBombe = generaBombe(16);
+    
     if(livello == "vuoto"){
         contenitore.innerHTML = " ";
     } else if (livello == "facile") {
@@ -23,26 +28,84 @@ play.addEventListener(`click`, function(){
     for( let cont = 0; cont < boxes.length; cont++ )
     {
         boxes[ cont ].addEventListener('click', function(){
-                
-                const contenuto = this.innerHTML
+            
+            const contenuto = this.innerHTML
+            
+            this.classList.add("blu");
+            
+            console.log( contenuto );
+            if(!gameOver){
+                const valoreCella = parseInt(this.querySelector("box").textContent);
+                if(arrayIncludeValore(listaPosizioniBombe, boxes)){
+                    this.classList.add("rosso");
 
-                console.log( contenuto );
-
-        })
+                    fineGioco();
+                }else{
+                    this.classList.add("blu");
+                }
+            }
+        });
+        console.log(boxes);
     }
-    // let bombeTotali = 16
-    // const posizioneBombe = [];
-    // while (posizioneBombe.length < bombeTotali){
-    //     const nuovaPosizioneBombe = generaRandomInt(1, celleTotali);
-    //     if(!arrayIncludeValore(posizioneBombe,nuovaPosizioneBombe)){
-    //         bombeGenerate.push(nuovaPosizioneBombe);
-    //     }
-    // }
-    //generiamo le bombe
-
+    
 });
 
-function creaBox( numeroRighe );
+function inizioGioco(){
+    console.log("INIZIO");
+}
+
+function fineGioco(){
+    gameOver = true
+    const listaCelle = document.querySelectorAll(".sqare") //modificare
+    for(let i=0; i<listaCelle.length; i++){
+        const cella = listaCelle[i];
+        const valoreCella = parseInt(cella.querySelector("box").textContent);
+        if(arrayIncludeValore(listaPosizioniBombe, valoreCella)){
+            cella.classList.add("rosso");
+        }
+    }
+    const gameOverElement = document.createElement("div");
+    gameOverElement.className = "game_over";
+    document.getElementsByTagName("body")[0].append(gameOverElement);
+    console.log("FINE");
+}
+
+tentativiPossibili = creaBox() - 16;
+
+
+    // generiamo le bombe
+
+function generaBombe(){
+    const posizioneBombeGenerate = [];
+    
+    while (posizioneBombeGenerate.length < 16){
+        const nuovaPosizioneBombe = generaRandomInt(1, creaBox());
+        if(!arrayIncludeValore(posizioneBombeGenerate,nuovaPosizioneBombe)){
+            posizioneBombeGenerate.push(nuovaPosizioneBombe);
+            console.log(nuovaPosizioneBombe);
+        }
+    }
+    console.log(posizioneBombeGenerate);
+    return posizioneBombeGenerate
+}
+
+function arrayIncludeValore(arrayValori, valore){
+    for(let i=0; i<arrayValori.length; i++){
+        if(arrayValori[i] == valore){
+            return true;
+        }
+    }
+    return false;
+}
+
+function generaRandomInt(min, max){
+   let result = Math.floor(Math.random()* (max-min +1)) + min;
+    return result
+}
+ // ciao
+
+
+function creaBox( numeroRighe )
 {
     let contatore = 1;
 
